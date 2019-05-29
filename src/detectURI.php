7b7a4a35989b27abd16df73ee8a2b397989b8a93
L!εsAμεr <liesauer@liesauer.net>
2019-05-29 09:41:42 +0800

    增强兼容性

--- src/detectURI.php
+++ src/detectURI.php
@@ -4,7 +4,8 @@ function detectURI()
 {
     /**
      * 如果PHP_SELF最后包含/，直接取QUERY_STRING
-     * 如有PATH_INFO、REDIRECT_URL，直接使用
+     * 如有PATH_INFO直接使用
+     * 如有REDIRECT_URL且DOCUMENT_ROOT + REDIRECT_URL不是有效的文件名则使用REDIRECT_URL
      * 如果QUERY_STRING带了问号，则取第一段使用
      * 如果DOCUMENT_ROOT + SCRIPT_NAME不是有效的文件名则使用SCRIPT_NAME
      */
@@ -13,7 +14,7 @@ function detectURI()
         $uri = $_SERVER['QUERY_STRING'];
     } else if (isset($_SERVER['PATH_INFO'])) {
         $uri = $_SERVER['PATH_INFO'];
-    } else if (isset($_SERVER['REDIRECT_URL'])) {
+    } else if (isset($_SERVER['REDIRECT_URL']) && !file_exists($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REDIRECT_URL'])) {
         $uri = $_SERVER['REDIRECT_URL'];
     } else if (isset($_SERVER['QUERY_STRING']) && strpos($_SERVER['QUERY_STRING'], '?') !== false) {
         $uri = explode('?', $_SERVER['QUERY_STRING'])[0];
